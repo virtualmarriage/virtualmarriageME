35a4f28a8a7fc4a6e6293056be48505dc75d640b
Anurag Katiyar <anurag@santosh.ac.in>
2020-10-30 19:40:53 +0530

    add dio library

--- lib/services/Api.dart
+++ lib/services/Api.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,13 +6,37 @@ import 'package:http/http.dart' as http;
 import 'package:meta/meta.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:virtualmarriageME/model/ProductData.dart';
-import 'package:virtualmarriageME/model/UserData.dart';
+import 'package:virtualmarriageME/model/UserResponse.dart';
+import 'package:virtualmarriageME/utils/CommonComponent.dart';
 import 'package:virtualmarriageME/utils/PreferenceHelper.dart';
 import 'package:virtualmarriageME/utils/progressdialog.dart';
 import 'package:virtualmarriageME/model/EarningData.dart';
+import 'package:dio/dio.dart';
 
 class Api {
-  final String _baseUrl = "http://yogihost.in/fueldynamic/";
+  final String _baseUrl = "https://virtualmarriage.me/api/v1/serviceProvider/";
+  ProgressDialog progressDialog;
+
+  Future<UserResponse> login({@required String mobileNo, @required BuildContext context}) async {
+    progressDialog= ProgressDialog(context, ProgressDialogType.Normal);
+    progressDialog.show();
+    try {
+      FormData formData = new FormData.fromMap({
+        "mobile": "$mobileNo",
+      });
+      print('Request: ${formData.fields}');
+      Response response = await Dio().post("${_baseUrl}login/", data: formData);
+      print(response);
+      progressDialog.hide();
+      return UserResponse.fromJson(response.data);
+    } catch (error, stacktrace) {
+      progressDialog.hide();
+      CommonComponent.showToast('$error');
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return error;
+    }
+  }
+
 
   Future<List<ProductData>> getProductList() async {
     var param = {
@@ -24,7 +47,7 @@ class Api {
     try {
       http.Response response = await http.post(url, body: param);
       if (response.statusCode == 200) {
-       /* var data = json.decode(response.body);
+        /* var data = json.decode(response.body);
         PreferenceHelper.saveValueInPreference(keyValue.userData.toString(), json.encode(data['userdata']));*/
 
         var data = json.decode(response.body);
@@ -36,7 +59,7 @@ class Api {
     return null;
   }
 
-  Future<String> login({@required String mobileNo, @required context}) async {
+  /*Future<String> login({@required String mobileNo, @required context}) async {
     ProgressDialog progressDialog = new ProgressDialog(context, ProgressDialogType.Normal);
     var param = {
       "api": 'login',
@@ -59,7 +82,7 @@ class Api {
       progressDialog.hide();
     }
     return null;
-  }
+  }*/
 
   Future<List<EarningData>> getEarningList() async {
     var param = {
@@ -118,25 +141,6 @@ class Api {
     }
     return null;
   }*/
-
-  _alert(BuildContext context, DialogType dialogType, String title, String message) {
-    AwesomeDialog(
-        context: context,
-        animType: AnimType.SCALE,
-        headerAnimationLoop: false,
-        dialogType: dialogType,
-        title: title,
-        desc: message,
-        btnOkColor: dialogType == DialogType.SUCCES ? Color(0xFF00CA71) : Colors.red,
-        btnOkOnPress: () {
-          debugPrint('OnClcik');
-        },
-        btnOkIcon: Icons.check_circle,
-        onDissmissCallback: () {
-          debugPrint('Dialog Dissmiss from callback');
-        })
-      ..show();
-  }
 
 /*Map<String, String> _headers() {
     return {
